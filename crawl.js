920702ae0ef52a85ed9958bc4c34f46550175b53
gwarzecha <gmwarzecha@tutanota.com>
2024-07-03 09:58:55 -0700

    Add JSDOM package and getURLsFromHTML function to ensure that href elements in a tags are absolute URLs, add tests to check for this

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+import { JSDOM } from 'jsdom';
+
 const normalizeURL = (rawUrl) => {
   const urlObj = new URL(rawUrl);
   let newURL = `${urlObj.host}${urlObj.pathname}`;
@@ -8,3 +10,24 @@ const normalizeURL = (rawUrl) => {
 };
 
 export { normalizeURL };
+
+const getURLsFromHTML = (htmlBody, baseURL) => {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const anchors = dom.window.document.querySelectorAll('a');
+
+  for (const anchor of anchors) {
+    if (anchor.hasAttribute('href')) {
+      let href = anchor.getAttribute('href');
+      try {
+        href = new URL(href, baseURL).href;
+        urls.push(href);
+      } catch (err) {
+        console.log(`${err.message}: ${href}`);
+      }
+    }
+  }
+  return urls;
+};
+
+export { getURLsFromHTML };
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@jest/globals';
-import { normalizeURL } from './crawl';
+import { getURLsFromHTML, normalizeURL } from './crawl';
 
 test('normalizeURL secure protocol without slash', () => {
   const input = 'https://blog.boot.dev/path';
@@ -27,4 +27,58 @@ test('normalizedURL unsecure protocol with slash', () => {
   const actual = normalizeURL(input);
   const expected = 'blog.boot.dev/path';
   expect(actual).toBe(expected);
+});
+
+test('normalizedURL caps', () => {
+  const input = 'HTTP://BLOG.BOOT.DEV/path';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toBe(expected);
+});
+
+test('Relative to absolute URLs', () => {
+  const htmlBody = `
+    <html>
+      <body>
+        <a href="https://example.com/page1">Example Page 1</a>
+        <a href="/page2">Example Page 2</a>
+        <a href="page3">Example Page 3</a>
+      </body>
+    </html>`;
+  const baseURL = 'https://example.com';
+
+  const expected = [
+    'https://example.com/page1',
+    'https://example.com/page2',
+    'https://example.com/page3',
+  ];
+
+  const actual = getURLsFromHTML(htmlBody, baseURL);
+  expect(actual).toEqual(expected);
+});
+
+test('Find all anchor elements', () => {
+  const htmlBody = `
+  <html>
+    <body>
+        <a href="https://example.com/page1">Example Page 1</a>
+        <a href="/page2">Example Page 2</a>
+        <a href="page3">Example Page 3</a>
+        <a href="//example.com/page4">Example Page 4</a>
+        <a href="https://example.com/page5">Example Page 5</a>
+      </body>
+  </html>
+  `;
+  const baseURL = 'https://example.com';
+
+  const expected = [
+    'https://example.com/page1',
+    'https://example.com/page2',
+    'https://example.com/page3',
+    'https://example.com/page4',
+    'https://example.com/page5',
+  ];
+
+  const actual = getURLsFromHTML(htmlBody, baseURL);
+  expect(actual).toEqual(expected);
 });
